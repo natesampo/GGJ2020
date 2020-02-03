0a565556680e3a7489c40b09319afe04bd9a48be
Nate Sampo <nathan.sampo@students.olin.edu>
2020-02-03 08:12:40 -0500

    one last push G

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ server.listen(process.env.PORT || port, function() {
 	}
 });
 
-var accessToken = '2MMTqs9tilAAAAAAAAAATI2Z_k9jW7qtzjRVhUuTOSy_E-9XYdmIv223Icj33TQ';
+var accessToken = '2MMTqs9tilAAAAAAAAAATSgeQuYDN3pbHX8HgplCZLo1jmJ5D_is12Bpeoq-ih8';
 
 io.on('connection', function(socket) {
 	console.log('New Connection');
